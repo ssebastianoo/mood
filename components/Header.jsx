f7a04f8ab8ef4d883b2283fe86feae5c080fa952
ssebastianoo <sebastianogirotto05@gmail.com>
2022-07-05 12:49:29 +0200

    home url

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,10 +4,15 @@ import Link from "next/link";
 export default function Header() {
     return (
         <div className="bg-purple-light w-full h-[8vh] flex items-center text-xl">
-            <div className="mx-5">
+            <div className="mx-3">
                 <Account />
             </div>
-            <div>
+            <div className="mx-3">
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+            </div>
+            <div className="mx-3">
                 <Link href="/stats">
                     <a>Stats</a>
                 </Link>
